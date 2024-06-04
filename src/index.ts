0def6a58a9f2682aa2293e67d3c1e8e7074b918e
N. Martin Abregu <nmabregu@gmail.com>
2024-06-03 21:39:19 -0300

    agrego model user

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose
+    .connect(process.env.MONGO_URI!)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
